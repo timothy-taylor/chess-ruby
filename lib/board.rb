52ebf29edf3bcf8c6c980a033acb30cc9694cf1c
Tim Taylor <timothy.george.taylor@gmail.com>
2020-11-30 13:51:17 -0500

    Added board setup methods

--- lib/board.rb
+++ lib/board.rb
@@ -5,6 +5,23 @@ module ChessSet
                    :bishop => "♗", :queen => "♕", :king => "♔" }
   BLACK_PIECES = { :pawn => "♟︎", :rook => "♜", :knight => "♞",
                    :bishop => "♝", :queen => "♛", :king => "♚" }
+  def setup(array, w = WHITE_PIECES, b = BLACK_PIECES)
+    black_back_row = [b[:rook], b[:knight], b[:bishop], b[:king], b[:queen],
+                      b[:bishop], b[:knight], b[:rook]]
+    black_front_row = Array.new(8, b[:pawn])
+    white_back_row = [w[:rook], w[:knight], w[:bishop], w[:king], w[:queen],
+                      w[:bishop], w[:knight], w[:rook]]
+    white_front_row = Array.new(8, w[:pawn])
+
+    populate_row(0, array, black_back_row)
+    populate_row(1, array, black_front_row)
+    populate_row(7, array, white_back_row)
+    populate_row(6, array, white_front_row)
+  end
+
+  def populate_row(row, array, set)
+    array[row].each_with_index { |e, i| array[row][i] = set[i] }
+  end
 end
 
 class Board
@@ -13,9 +30,12 @@ class Board
   attr_accessor :board
 
   def initialize
-    pp @board = Array.new(8, Array.new(8, 0))
+    @board = Array.new(8) {Array.new(8, "-")}
+    setup(@board)
+    pp @board
   end
 
+  
   def allowable_move?(position)
     if position[0] < 0 || position[1] < 0
       false
--- lib/play.rb
+++ lib/play.rb
@@ -4,5 +4,5 @@ move_1 = [ rand(8), rand(8) ]
 move_2 = [ rand(8), rand(8) ]
 
 board = Board.new
-player = board.create_knight
-player.knight_moves(move_1, move_2)
+#player = board.create_knight
+#player.knight_moves(move_1, move_2)
